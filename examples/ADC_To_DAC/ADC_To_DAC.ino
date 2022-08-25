929dd0636c0a593cf14a9c4e26dce6f88da5da6b
iabdalkader <i.abdalkader@gmail.com>
2022-08-25 12:16:21 +0200

    Update examples.

--- examples/ADC_To_DAC/ADC_To_DAC.ino
+++ examples/ADC_To_DAC/ADC_To_DAC.ino
@@ -1,23 +1,22 @@
+// This example outputs an 8KHz square wave on DAC1.
+// Connect ADC channel A0 to VDD and channel A1 to GND.
+// Note DAC's sample size is double, because ADC samples 2 channels,
 #include "AdvancedADC.h"
 #include "AdvancedDAC.h"
 
-AdvancedADC adc0(A0);
+AdvancedADC adc0(A0, A1);
 AdvancedDAC dac1(A12);
 
 void setup() {
     Serial.begin(9600);
 
-    while (!Serial) {
-
-    }
-
     // Resolution, sample rate, number of samples per channel, queue depth.
-    if (!adc0.begin(ADC_RESOLUTION_12, 16000, 32, 64)) {
+    if (!adc0.begin(ADC_RESOLUTION_12, 8000, 32, 64)) {
         Serial.println("Failed to start analog acquisition!");
         while (1);
     }
 
-    if (!dac1.begin(DAC_RESOLUTION_12, 8000, 32, 64)) {
+    if (!dac1.begin(DAC_RESOLUTION_12, 8000, 32 * 2, 64)) {
         Serial.println("Failed to start analog acquisition!");
         while (1);
     }
@@ -25,7 +24,6 @@ void setup() {
 
 void loop() {
     if (adc0.available()) {
-        SampleBuffer buf = adc0.read();
-        dac1.write(buf);
+        dac1.write(adc0.read());
     }
 }
