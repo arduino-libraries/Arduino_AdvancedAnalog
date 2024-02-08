a3bc455113a7f705168188eddf1542852b79aef7
Leonardo Cavagnis <45899760+leonardocavagnis@users.noreply.github.com>
2024-02-08 09:57:15 +0100

    Update src/AdvancedADC.cpp
    Co-authored-by: Ibrahim Abdelkader <i.abdalkader@gmail.com>

--- src/AdvancedADC.cpp
+++ src/AdvancedADC.cpp
@@ -115,7 +115,7 @@ DMABuffer<Sample> &AdvancedADC::read() {
     return NULLBUF;
 }
 
-int AdvancedADC::begin(uint32_t resolution, uint32_t sample_rate, size_t n_samples, size_t n_buffers,bool noStart) {
+int AdvancedADC::begin(uint32_t resolution, uint32_t sample_rate, size_t n_samples, size_t n_buffers, bool start) {
     
     ADCName instance = ADC_NP;
     // Sanity checks.
